aa0deab7ee7c3d2bcdf9bb8bea3585634f8679b5
zephyr5028 <marguerie.jl@wanadoo.fr>
2016-12-08 20:09:05 +0100

    nettoyage

--- porte_poulailler_affichage_clavier_rtc/radio.h
+++ porte_poulailler_affichage_clavier_rtc/radio.h
@@ -13,13 +13,8 @@ void envoiMessage(char chaine1[]) {
       Serial.println(chaine);
     }
   } else {
-     messageRadio(chaine);
-/*    strcat(chaine, '\0');
-    vw_send((uint8_t *)chaine, strlen(chaine) + 1); // On envoie le message
-    // strlen : Retourne le nombre de caractères de cs sans tenir compte du caractère de fin de chaîne.
-    vw_wait_tx(); // On attend la fin de l'envoi
-    delay(10);
-    chaine[0] = '\0';*/
+    messageRadio(chaine);
+    chaine[0] = '\0';
   }
 }
 
@@ -30,10 +25,6 @@ void messageRadio(char chaine1[]) {
   // strlen : Retourne le nombre de caractères de cs sans tenir compte du caractère de fin de chaîne.
   vw_wait_tx(); // On attend la fin de l'envoi
   delay(10);
-  chaine[0] = '\0';
-  //for (byte i=0;i<=taille;chaine[i++]=0) {
-  // }
-
 }
 
 //----chaine radio fin de ligne avant transmission
