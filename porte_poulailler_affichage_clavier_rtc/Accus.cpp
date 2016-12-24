1615b24dfd24d405dbd3906f9f2ec38a7a607c41
zephyr5028 <marguerie.jl@wanadoo.fr>
2016-12-24 17:27:49 +0100

    declaration classe accus

---
+++ porte_poulailler_affichage_clavier_rtc/Accus.cpp
@@ -0,0 +1,21 @@
+/* Accus.cpp
+  définitions de la classe Accus
+  bloc accus NIMH
+*/
+
+#include "Accus.h"
+
+//constructeur avec debug
+Accus::Accus( boolean debug) :  m_debug(debug)
+{
+
+}
+
+Accus::~Accus()
+{
+}
+
+void Accus::init() {
+  
+}
+
